2fbe57a9b8bc9e94725bf36c09e8c679c30f5017
Nasheeda03 <nasheedaparveen.ep@rariti.io>
2024-02-28 00:05:25 +0530

    RADE-1041: Completed the UI for hackathon deploy app

---
+++ src/enums/appEnum.js
@@ -0,0 +1,10 @@
+export const APP_ENUM = {
+  connectWallet: "Connect Wallet",
+  deployContent: "Deploy Your Rollup!",
+  organization: "Oganization",
+  rollupName: "Rollup name",
+  chainId: "Chain ID",
+  customizeStack: "Customise your stack",
+  dataAvailability: "Data Availability",
+  environment: "Environment",
+};
